684e38cd0d95e789ae9d71e2ebb96eac64e4438f
Anuj Sundriyal <anujsundriyal1234@gmail.com>
2022-05-12 09:02:45 +0530

    final added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+// import React,{ useState,useEffect} from 'react'
+// import{BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Crousel from './Crousel.js'
+import Navbar from './Navbar.js'
 
-function App() {
+import Latest from './Latest.js'
+import About from './About.js';
+import Signup from './Signup.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+ 
+} from "react-router-dom";
+import Login from './Login.js';
+import BB from './BB.js';
+import Service from './Service.js';
+
+import Final from './Final.js';
+import FooterPage from './Footer.js';
+import MyCarousel from './MyCarousel.js';
+import Forget from './Forget.js';
+import Registration from './Registration.js';
+import Login1 from './Login1.js';
+export default function App() {
+ 
+  const d={
+    backgroundImage:"linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url('http://www.indiantrekking.com/themes/images/slider2.jpg')",
+    height:"75vh",
+    backgroundSize:'cover',
+    backgroundRepeat:'no-repeat',
+   backgroundBlendMode:'darken'
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div style={d} id="ds">
+    <Router>
 
-export default App;
+     <Navbar/>
+      
+    <Switch>
+    <Route exact path="/">
+      <Crousel />
+      <About/>
+<Latest/>
+
+      
+
+      </Route>
+      <Route  path="/login">
+       <Login/>
+      </Route>
+      <Route  path="/login1">
+       <Login1/>
+      </Route>
+      <Route path="/Service">
+       <Service/>
+      </Route>
+      <Route path="/Final">
+       <Final/>
+      </Route>
+      <Route path="/Signup">
+       <Signup/>
+      </Route>
+      <Route path="/Features">
+        <MyCarousel/>
+      </Route>
+      <Route path="/Forget">
+        <Forget/>
+      </Route>
+      <Route path="/Registration">
+        <Registration/>
+      </Route>
+      <Route path="/BB">
+        <BB/>
+      </Route>
+    
+    </Switch>
+    </Router>
+    <FooterPage/>
+    </div>
+  )
+}
+  
